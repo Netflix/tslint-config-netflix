f8fea2965bfb696410301d09d8be2d9b155b9d86
Kaylie Kwon <kkwon@netflix.com>
2018-08-28 15:05:15 -0700

    Add tslint configuration

---
+++ index.js
@@ -0,0 +1,40 @@
+module.exports = {
+    rules: {
+        "align": [true, "parameters", "statements"],
+        "arrow-return-shorthand": true,
+        'curly': true,
+        'max-line-length': [true, 120],
+        'new-parens': true,
+        'no-arg': true,
+        'no-bitwise': true,
+        'no-conditional-assignment': true,
+        'no-duplicate-imports': true,
+        'no-internal-module': true,
+        'no-inferrable-types': true,
+        'no-multi-spaces': true,
+        'no-parameter-reassignment': true,
+        'no-string-literal': true,
+        'no-trailing-whitespace': true,
+        'no-var-keyword': true,
+        'no-unnecessary-initializer': true,
+        'ordered-imports': true,
+        'prefer-const': true,
+        'prefer-readonly': true,
+        'quotemark': [true, 'single', 'jsx-double'],
+        'semicolon': [true, 'always'],
+        'trailing-comma': [true, {'multiline': 'always', 'singleline': 'never'}],
+        'triple-equals': [true, 'allow-null-check'],
+        'whitespace': [
+            true,
+            'check-branch',
+            'check-decl',
+            'check-module',
+            'check-operator',
+            'check-separator',
+            'check-type',
+            'check-typecast',
+            'check-type-operator',
+            'check-rest-spread'
+        ]
+    }
+}
